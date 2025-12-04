ac05edcb416b96921e5dd192fcc202d15115e7ec
Arut0ria <anoxing64@gmail.com>
2025-12-04 21:43:48 +0100

    fix(Project): :bookmark: Removing external for build config

--- vite.config.ts
+++ vite.config.ts
@@ -25,7 +25,6 @@ export default defineConfig({
   },
   build: {
     rollupOptions: {
-      external: ['react', 'react-dom'],
       output: {
         manualChunks(id) {
           if (id.includes('node_modules')) {
